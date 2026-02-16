149a808345eb0583f89ebe9b429f9a068fbec38c
Prabhat <osrprabhatyadav4@gmail.com>
2026-02-16 22:12:31 +0530

    BASICS: added dataTypes.

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3 + 3)   // we are using nodejs, not browser
+
+console.log(3
+    + 3         // code readability should be high
+)
+
+console.log("Prabhat")
+
+let name = "Prabhat"
+let age = 21
+let isLoggedIn = false
+
+// number => 2^53
+// bigint 
+// string =? " " / ' '
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol
+// object
+
+console.log(typeof name);
+console.log(typeof "1234");
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
