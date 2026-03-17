954dac01b914ea844ed093436c36a74aa6900699
Prabhat <osrprabhatyadav4@gmail.com>
2026-03-17 13:08:58 +0530

    PROJECTS: added 2 projects of Async

---
+++ 08_PROJECTS/06_UnlimitedColors/script.js
@@ -0,0 +1,27 @@
+const randomColor = function () {
+    const hex = '0123456789ABCDEF'  
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+function changeBgColor() {
+    document.body.style.backgroundColor = randomColor()
+}
+
+let interval;
+
+const startChangingColor = function(){
+    if(!interval){
+        interval = setInterval(changeBgColor, 1000)
+    }
+}
+const stopChangingColor = function(){
+    clearInterval(interval)
+    interval = null
+}
+
+document.querySelector('#start').addEventListener("click", startChangingColor)
+document.querySelector('#stop').addEventListener("click", stopChangingColor)
